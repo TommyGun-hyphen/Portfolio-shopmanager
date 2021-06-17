576a9f857a361410c3459bbfed5c6c055e324392
TommyGun-hyphen <75859933+TommyGun-hyphen@users.noreply.github.com>
2021-06-17 21:01:29 +0100

    Add files via upload

--- cart.php
+++ cart.php
@@ -10,7 +10,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Cart</title>
 </head>
 <body">
     
@@ -23,11 +23,14 @@ session_start();
     <?php
         $products = [];
         $total_price = 0;
-        foreach($_SESSION['items'] as $item_id){
-            $product = ProductsRepository::getById($item_id);
-            $total_price = $total_price + $product->getPrice();
-            $products[] = $product;
+        if(isset($_SESSION['items'])){
+            foreach($_SESSION['items'] as $item_id){
+                $product = ProductsRepository::getById($item_id);
+                $total_price = $total_price + $product->getPrice();
+                $products[] = $product;
+            }
         }
+        
 
 
     ?>
