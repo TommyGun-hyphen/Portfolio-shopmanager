2f5cf4f53495839bde0c7e376b2184afc3eba225
TommyGun-hyphen <75859933+TommyGun-hyphen@users.noreply.github.com>
2021-06-17 19:25:59 +0100

    Add files via upload

---
+++ admin/partials/header.php
@@ -0,0 +1,39 @@
+<header>
+<nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background:#27233A">
+    <a href="#" class="navbar-brand h2">SHOP ADMIN</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarContent">
+        <ul class="navbar-nav ml-auto form-inline">
+            <li class="nav-item" type="button">
+            <div class="nav-item dropdown w-100">
+                <a class="dropdown-toggle text-white font-weight-bold nav-link" role="button" id="#manage" data-toggle="dropdown" aria-expanded="false">
+                    MANAGE
+                </a>
+
+                <ul class="dropdown-menu" aria-labelledby="manage">
+                    <li><a class="dropdown-item" href="/shopmanager/admin/products.php">PRODUCTS</a></li>
+                    <li><a class="dropdown-item" href="/shopmanager/admin/add_product.php">ADD PRODUCT</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="/shopmanager/admin/clients.php">CLIENTS</a></li>
+                    <li><a class="dropdown-item" href="/shopmanager/admin/add_client.php">ADD CLIENT</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">ORDERS</a></li>
+                </ul>
+            </div>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link text-light font-weight-bold">USERS</a>
+            </li>
+
+            <li class="nav-item">
+                <a href="" class="nav-link text-light font-weight-bold">{{USERNAME}}</a>
+            </li>
+            
+        </ul>
+    </div>
+    
+</nav>
+</header>
+<div class="mt-5 pt-5"></div>
